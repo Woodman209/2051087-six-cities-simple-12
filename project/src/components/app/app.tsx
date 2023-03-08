6705cf35827afb53ed03304d9f46f90da773cdc7
Nikita <tereshkonikita@mail.ru>
2023-03-08 18:41:48 +0300

    Добавляет базовый роутинг

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainScreen from '../../pages/main-screen/main-screen';
+// import MainEmptyScreen from '../../pages/main-empty-screen/main-empty-screen';
+// import PropertyNotLoggedScreen from '../../pages/property-not-logged-screen/property-not-logged-screen';
+import PropertyScreen from '../../pages/property-screen/property-screen';
+import ErrorNotFound from '../../pages/404-screen/404-screen';
+import { LoginScreen } from '../../pages/login-screen/login-screen';
+import { AppRoute } from '../../const';
 
 type RentCount = {
   count: number;
@@ -6,7 +13,26 @@ type RentCount = {
 
 function App({ count }: RentCount): JSX.Element {
   return (
-    <MainScreen count={count}></MainScreen>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen count={count}></MainScreen>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={`${AppRoute.Room}:id`}
+          element={<PropertyScreen />}
+        />
+        <Route
+          path={AppRoute.NotFound}
+          element={<ErrorNotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
