290f07d56b40c58de333a471bbc513383ccb8596
Nikita <tereshkonikita@mail.ru>
2023-03-28 22:12:45 +0300

    Вносит правки

--- project/src/store/mock.ts
+++ project/src/store/mock.ts
@@ -1,3 +1,6 @@
 import { UserLogin } from '../types/type';
+import { offers } from '../mocks/offers';
+
+export const [{ city: currentCity }] = offers;
 
 export const userLogin: UserLogin = 'fake@fake.fake';
