3da086db8ed3cca8fc9b7cfc56830df0a517e2d7
Nikita <tereshkonikita@mail.ru>
2023-03-28 21:33:20 +0300

    Добавляет Action-Creator changeCity

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,3 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
+import { CityName } from '../types/type';
 
 export const resetCity = createAction('city/reset');
+
+export const changeCity = createAction<CityName>('city/change');
+
